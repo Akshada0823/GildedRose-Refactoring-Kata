22028c5e30a6c8edde282434af842d85d041f0a5
Akshada0823 <akshadasakhare12@gmail.com>
2026-02-05 20:46:52 +0200

    added new functionality conjured

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,109 +3,66 @@ package com.gildedrose
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
-//        for (i in items.indices) {
-//            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-//                if (items[i].quality > 0) {
-//                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-//                        items[i].quality = items[i].quality - 1
-//                    }
-//                }
-//            } else {
-//                if (items[i].quality < 50) {
-//                    items[i].quality = items[i].quality + 1
-//
-//                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-//                        if (items[i].sellIn < 11) {
-//                            if (items[i].quality < 50) {
-//                                items[i].quality = items[i].quality + 1
-//                            }
-//                        }
-//
-//                        if (items[i].sellIn < 6) {
-//                            if (items[i].quality < 50) {
-//                                items[i].quality = items[i].quality + 1
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-//                items[i].sellIn = items[i].sellIn - 1
-//            }
-//
-//            if (items[i].sellIn < 0) {
-//                if (items[i].name != "Aged Brie") {
-//                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-//                        if (items[i].quality > 0) {
-//                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-//                                items[i].quality = items[i].quality - 1
-//                            }
-//                        }
-//                    } else {
-//                        items[i].quality = items[i].quality - items[i].quality
-//                    }
-//                } else {
-//                    if (items[i].quality < 50) {
-//                        items[i].quality = items[i].quality + 1
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-                items.forEach { item ->
-
-                    when (item.name) {
-
-                        "Sulfuras, Hand of Ragnaros" -> {
-                            // Legendary item — nothing changes
-                        }
-
-                        "Aged Brie" -> {
-                            increaseQuality(item)
-                            item.sellIn--
-
-                            if (item.sellIn < 0) {
-                                increaseQuality(item)
-                            }
-                        }
-
-                        "Backstage passes to a TAFKAL80ETC concert" -> {
-                            increaseQuality(item)
-
-                            when {
-                                item.sellIn < 6 -> repeat(2) { increaseQuality(item) }
-                                item.sellIn < 11 -> increaseQuality(item)
-                            }
-
-                            item.sellIn--
-
-                            if (item.sellIn < 0) {
-                                item.quality = 0
-                            }
-                        }
-
-                        else -> {
-                            decreaseQuality(item)
-                            item.sellIn--
-
-                            if (item.sellIn < 0) {
-                                decreaseQuality(item)
-                            }
-                        }
-                    }
+        items.forEach { item ->
+            when (item.name) {
+                "Aged Brie" -> updateAgedBrie(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePass(item)
+                "Sulfuras, Hand of Ragnaros" -> { /* legendary, no-op */
                 }
-            }
 
-            private fun increaseQuality(item: Item) {
-                if (item.quality < 50) item.quality++
+                else -> updateNormalorConjuredItem(item)
             }
+        }
 
-            private fun decreaseQuality(item: Item) {
-                if (item.quality > 0) item.quality--
-            }
+    }
 
+
+    private fun updateNormalorConjuredItem(item: Item) {
+
+        val degradeRate = if (item.name.startsWith("Conjured")) 2 else 1
+
+        repeat(degradeRate) { decreaseQuality(item) }
+
+        item.sellIn--
+
+        if (item.sellIn < 0) {
+            repeat(degradeRate) { decreaseQuality(item) }
+        }
+    }
+
+    private fun updateAgedBrie(item: Item) {
+        increaseQuality(item)
+        item.sellIn--
+
+        if (item.sellIn < 0) {
+            increaseQuality(item)
+        }
+    }
+
+    private fun updateBackstagePass(item: Item) {
+        increaseQuality(item)
+
+        if (item.sellIn < 11) increaseQuality(item)
+        if (item.sellIn < 6) increaseQuality(item)
+
+        item.sellIn--
+
+        if (item.sellIn < 0) {
+            item.quality = 0
+        }
+    }
+
+    private fun decreaseQuality(item: Item) {
+        if (item.quality > 0) {
+            item.quality--
+        }
+    }
+
+    private fun increaseQuality(item: Item) {
+        if (item.quality < 50) {
+            item.quality++
+        }
+    }
 
 
 }
