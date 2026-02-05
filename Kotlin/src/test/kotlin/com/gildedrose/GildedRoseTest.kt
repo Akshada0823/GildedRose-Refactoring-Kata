97654f2e4a26a37fc1aeb9027f4e8b01cbe35d3c
Akshada0823 <akshadasakhare12@gmail.com>
2026-02-05 20:47:21 +0200

    added test cases

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -14,6 +14,41 @@ internal class GildedRoseTest {
 
     }
 
+    @Test
+    fun `conjured item degrades twice as fast before sell date`() {
+
+        val items = listOf(Item("Conjured Mana Cake", 10, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(18, items[0].quality)
+        assertEquals(9, items[0].sellIn)
+    }
+
+    @Test
+    fun `conjured item degrades four times as fast after sell date`() {
+
+        val items = listOf(Item("Conjured Mana Cake", 0, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(16, items[0].quality)
+        assertEquals(-1, items[0].sellIn)
+    }
+
+    @Test
+    fun `conjured item quality never negative`() {
+
+        val items = listOf(Item("Conjured Mana Cake", 5, 1))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(0, items[0].quality)
+    }
+
 }
 
 
